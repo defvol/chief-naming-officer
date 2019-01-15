572d091a34ca133290333b1aad7a78e22c517846
rodowi <rwilhelmy@gmail.com>
2019-01-14 23:07:22 -0800

    Get dictionary of transitions

--- test/utils.test.js
+++ test/utils.test.js
@@ -9,6 +9,18 @@ test('ngrams', function (t) {
   t.end()
 })
 
+test('transitions', function (t) {
+  const text = 'it is what it is supposed to be, is it?'
+  const ngrams = utils.ngrams(text, 3)
+  const got = utils.transitions(ngrams)
+  t.deepEqual(got['is '], ['wha'])
+  t.deepEqual(got['e, '], ['is '])
+  t.deepEqual(got['t i'], ['t i', 's s'])
+  t.equal(ngrams.length, 12)
+  t.equal(Object.keys(got).length, 10)
+  t.end()
+})
+
 test('usage', function (t) {
   var got = utils.usage()
   t.true(got.match(/usage/), 'returns some instructions')
