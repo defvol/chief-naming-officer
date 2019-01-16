cdc96b4da0d6eb1ea13a3a0e780c5aa2d8741b0e
rodowi <rwilhelmy@gmail.com>
2019-01-15 22:37:16 -0800

    Make readFile a utils

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,6 @@
-const fs = require('fs')
 const mgen = require('../lib/index')
+const { readFile } = require('../lib/utils')
 const test = require('tape')
-
-function readFile (fname) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(fname, (err, res) => {
-      if (err) return reject(err)
-      const text = res.toString()
-        .replace(/\n/g, ' ')
-        .trim()
-      resolve(text)
-    })
-  })
-}
 
 test('index', async (t) => {
   const text = await readFile('./test/a.txt')
